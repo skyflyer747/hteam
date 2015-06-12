339947707bb5bb2cac39ed9ccaf275b0edf06435
SquishyStrawberry <therealrebuilder@gmail.com>
2015-06-12 16:54:03 +0200

    (Game) Added another br.

--- scripts/game.js
+++ scripts/game.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
     function progress(percent, $element) {
         var progressBarWidth = percent * $element.width() / 100;
-        $element.find(‘div’).animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
+        $element.find("div").animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
     }
 
     function moneyClick(millis) {
