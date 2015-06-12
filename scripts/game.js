d67dd03b8d9c0142d18fe7f3e24e271c1653a4f6
SquishyStrawberry <therealrebuilder@gmail.com>
2015-06-12 05:27:22 +0200

    I'm quite an idiot.

---
+++ scripts/game.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var gameState = {
+        "money": 0,
+        "areas": 0
+    };
+    $("#moneyButton").click(function() {
+        gameState.money += 1;
+    });    
+
+    function updateDisplay() {
+        $("#moneyDisplay").html("€" + gameState.money);
+    };
+
+    setinterval(updateDisplay, 100);
+});
