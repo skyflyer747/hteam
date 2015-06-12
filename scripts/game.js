42da2fa9a54bb1f03e7c2c07d4eedac21170823f
SquishyStrawberry <therealrebuilder@gmail.com>
2015-06-12 16:51:48 +0200

    (Game) Progress bar stuffsies.

--- scripts/game.js
+++ scripts/game.js
@@ -3,15 +3,29 @@ $(document).ready(function() {
         "money": 0,
         "areas": 0
     };
-    $("#moneyButton").click(function() {
-        
-        gameState.money += 1;
-    });    
+
+
 
     function updateDisplay() {
         $("#moneyDisplay").html("€" + gameState.money);
     };
 
+    function progress(percent, $element) {
+        var progressBarWidth = percent * $element.width() / 100;
+        $element.find(‘div’).animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
+    }
 
+    function moneyClick(millis) {
+        if (millis === null) {
+            millis = 1000;
+        }
+        else if (millis < 1) {
+            return;    
+        }
+        progress(millis/100, $("#exploreBar"));
+        setTimeout(function () {moneyClick(millis-100)}, 100);
+    }
+
+    $("#moneyButton").click(moneyClick);
     setInterval(updateDisplay, 100);
 });
