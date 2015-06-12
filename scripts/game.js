893b72e7dace10eb2e0221eff6da4d544a4ea751
SquishyStrawberry <therealrebuilder@gmail.com>
2015-06-12 17:53:17 +0200

    (Game) Added check for bar already filling.

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     var gameState = {
+        // Represents Areas.
         "money": 0,
-        "areas": 0,
         // Delay between progress bar increments.
         "incrDel": 100,
         // Initial progress bar filling.
         "startProgr": 0,
+        // Self explanatory.
+        "barFilling": false,
     };
 
 
@@ -18,16 +20,22 @@ $(document).ready(function() {
         var progressBarWidth = percent * $element.width() / 100;
         $element.find("div").animate({ width: progressBarWidth }, time || 500);
         if (typeof afterwards === "function") {
-            afterwards()
+            setTimeout(afterwards, time || 500);
         }
     }
 
     function moneyClick() {
+        // Keeps you from clicking more than once.
+        if (gameState.barFilling) {
+            return;
+        }
+        gameState.barFilling = true;
         progress(100, 
-                 $("#exploreBar"), 
+                 $("#exploreBar"),
                  gameState.incrDel*100,
                  function() {
                     gameState.money += 1;
+                    gameState.barFilling = false;
                     progress(0,
                              $("#exploreBar"),
                              10);
